ad952158a3489c8cb609d7afedf13b24ed54d622
SOLP\zeeoe <solp97@naver.com>
2022-09-22 18:11:10 +0900

    [Add] Interact 추가

--- MiniGame/Assets/Scripts/Player/PlayerInput.cs
+++ MiniGame/Assets/Scripts/Player/PlayerInput.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class PlayerInput : MonoBehaviour
 {
     // PlayerMove 스크립트에서 받아 올 좌 우 키값 변수
     public float MoveX;
+
+    [SerializeField]
+    private PlayerInteract _PlayerInteract;
+
+    
 
     // 일단 대략적으로 한 것이니 본인 입맛대로 수정 하셔도 됩니다. 대신, 수정하실 때는 팀원을에게 꼭 말씀해주세요
     private void FixedUpdate()
@@ -13,7 +20,7 @@ public class PlayerInput : MonoBehaviour
         // 상호작용 키 눌렀을 경우
         if(Input.GetKeyDown(KeyCode.E))
         {
-
+            _PlayerInteract.InteractObject();
         }
 
         // 점프 키 눌렀을 경우
