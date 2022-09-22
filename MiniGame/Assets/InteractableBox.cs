82814f5dc6223b62db55a35507f524124ebdbf20
SOLP\zeeoe <solp97@naver.com>
2022-09-22 23:27:05 +0900

    [Complete] Interactable Scripts

--- MiniGame/Assets/InteractableBox.cs
+++ MiniGame/Assets/InteractableBox.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class InteractableBox : Interactable
 {
+    //여기서 오브젝트 이벤트 실행
     public override void ActivateObject()
     {
         Destroy(this.gameObject);
-
+        Debug.Log("앙사라짐");
     }
 }
--- MiniGame/Assets/Scripts/Player/PlayerInteract.cs
+++ MiniGame/Assets/Scripts/Player/PlayerInteract.cs
@@ -33,14 +33,16 @@ public class PlayerInteract : MonoBehaviour
     //트리거 안에 들어왔을때 상호작용이 되는 레이어인가 비교 후 패널 팝업
     private void OnTriggerStay2D(Collider2D collision)
     {
+        //해당 오브젝트의 정보를 가지고 옴
         if(collision.GetComponent<Interactable>()!=null)_tempInteractable = (Interactable)collision.GetComponent<Interactable>();
-
+        //상호작용이 가능한 태그를 가지고있을시 이름패널 팝업
             if (collision.gameObject.CompareTag("Interact"))
             {
                 IsInteractOK = true;
                 PopupObject.SetActive(true);
             }
     }
+    //나가면 모두 꺼짐
     private void OnTriggerExit2D(Collider2D collision)
     { 
             IsInteractOK = false;
