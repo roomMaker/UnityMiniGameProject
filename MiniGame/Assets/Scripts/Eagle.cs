276bb7c3a465e79646f9edff78123259b6e393df
shinshinjin <sinhukjin01@naver.com>
2022-09-22 18:10:50 +0900

    Feat. Add Eagle Script
    Feat. Add Eagle Script

--- MiniGame/Assets/Scripts/Eagle.cs
+++ MiniGame/Assets/Scripts/Eagle.cs
@@ -4,9 +4,22 @@ using UnityEngine;
 
 public class Eagle : MonoBehaviour
 {
+    private float elapsedTime = 0f;
 
-    void Update()
+    private void Start()
     {
-        
+        StartCoroutine(EAGLE_Updown());
     }
+    IEnumerator EAGLE_Updown()
+    {
+        elapsedTime += Time.deltaTime;
+        if(elapsedTime > 1f)
+        {
+            elapsedTime = 0f;
+            gameObject.transform.position = Vector2.left * elapsedTime;
+        }
+
+        yield return new WaitForSeconds(elapsedTime);
+    }
+
 }
--- MiniGame/Assets/Scripts/Player/PlayerInput.cs
+++ MiniGame/Assets/Scripts/Player/PlayerInput.cs
@@ -7,29 +7,26 @@ public class PlayerInput : MonoBehaviour
     // PlayerMove 스크립트에서 받아 올 좌 우 키값 변수
     public float MoveX;
 
+    [SerializeField]
+    private PlayerInteract _PlayerInteract;
     // 일단 대략적으로 한 것이니 본인 입맛대로 수정 하셔도 됩니다. 대신, 수정하실 때는 팀원을에게 꼭 말씀해주세요
     private void FixedUpdate()
     {
-
         // 상호작용 키 눌렀을 경우
-        if(Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E))
         {
-           
+            _PlayerInteract.InteractObject();
         }
 
-        // 점프 키 눌렀을 경우
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-         
-        }
+        // 점프 키 처리 삭제 => PlayerMove 스크립트로 이동
 
         // 게임오버 처리
-        if(Input.GetKeyDown(KeyCode.R))
+        if (Input.GetKeyDown(KeyCode.R))
         {
 
         }
 
         // 좌 우 값 받아서 MoveX에 업데이트
-        MoveX = Input.GetAxis("Horizontal");
+        MoveX = Input.GetAxisRaw("Horizontal");
     }
 }
