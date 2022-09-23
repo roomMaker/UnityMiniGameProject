dd68d96aa0bb229003f48c03a85ef468a3512526
SOLP\zeeoe <solp97@naver.com>
2022-09-23 13:35:23 +0900

    [Fix]FallingTrigger&Input Scripts

--- MiniGame/Assets/Scripts/Object/FallingTrigger.cs
+++ MiniGame/Assets/Scripts/Object/FallingTrigger.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class FallingTrigger : MonoBehaviour
 {
     //떨어지는 오브젝트
-    public GameObject FallingObject;
+    public GameObject[] FallingObjects;
 
-    private void OnTriggerStay2D(Collider2D other)
+    //트리거에 닿으면 오브젝트가 떨어짐
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        //트리거에 닿으면 오브젝트가 떨어짐
-        if (other.gameObject.CompareTag("Player")) FallingObject.SetActive(true);
+        
+        if (other.gameObject.CompareTag("Player")) FallingObjects[0].SetActive(true);
     }
 
 }
